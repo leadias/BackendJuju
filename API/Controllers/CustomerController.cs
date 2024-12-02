c94ffe4d2dd5c6eeee7bcd9d51e94903cbd8df3b
unknown <lea-78@hotmail.es>
2024-12-02 11:21:39 -0500

    Creacion inicial proyecto

---
+++ API/Controllers/CustomerController.cs
@@ -0,0 +1,104 @@
+﻿using Business;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CustomerEntity = DataAccess.Data.Customer;
+using PostEntity = DataAccess.Data.Post;
+
+namespace API.Controllers.Customer
+{
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private BaseService<CustomerEntity> CustomerService;
+        private BaseService<PostEntity> PostService;
+
+        public CustomerController(BaseService<CustomerEntity> customerService, BaseService<PostEntity> postService)
+        {
+            CustomerService = customerService;
+            PostService = postService;
+        }
+
+
+        [HttpGet()]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                IQueryable<CustomerEntity> list  = CustomerService.GetAll();
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = list});
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
+
+        }
+
+
+        [HttpPost()]
+        public IActionResult Create([FromBodyAttribute] CustomerEntity entity)
+        {
+
+            try
+            {
+                var customers = CustomerService.GetAll();
+
+                foreach (var item in customers)
+                {
+                    if (item.Name == entity.Name)
+                    {
+                        return BadRequest("you already mentioned the name.");
+                    }
+                }
+
+                CreateCustomer(entity);
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = "Customer create" });
+
+            } catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
+
+
+        }
+
+        private CustomerEntity CreateCustomer(CustomerEntity entity)
+        {
+            return CustomerService.Create(entity);
+        }
+
+        [HttpPut()]
+        public CustomerEntity Update(CustomerEntity entity)
+        {
+            return CustomerService.Update(entity.CustomerId, entity, out bool changed);
+        }
+
+        [HttpDelete()]
+        public IActionResult Delete([FromBodyAttribute] CustomerEntity entity)
+        {
+            try
+            {
+                var post = PostService.GetAll();
+                var postUser = post.Where(x => x.CustomerId == entity.CustomerId).ToList();
+
+                foreach (var item in postUser)
+                {
+                    PostService.Delete(item);
+                }
+
+                CustomerService.Delete(entity);
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = "Customer delete" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
+
+        }
+    }
+}
---
+++ API/Controllers/PostController.cs
@@ -0,0 +1,82 @@
+﻿using Business;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PostEntity = DataAccess.Data.Post;
+using CustomerEntity = DataAccess.Data.Customer;
+using Microsoft.AspNetCore.Http;
+using System;
+using DataAccess.Data;
+
+namespace API.Controllers.Post
+{
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        private BaseService<PostEntity> PostService;
+        private BaseService<CustomerEntity> CustomerService;
+        public PostController(BaseService<PostEntity> postService, BaseService<CustomerEntity> customerService)
+        {
+            PostService = postService;
+            CustomerService = customerService;
+        }
+
+        [HttpGet()]
+        public IQueryable<PostEntity> GetAll()
+        {
+            return PostService.GetAll();
+        }
+
+        [HttpPost()]
+        public IActionResult Create([FromBodyAttribute]  PostEntity entity)
+        {
+            try
+            {
+                var user = CustomerService.GetAsync(entity.CustomerId);
+                if (user == null)
+                {
+                    return BadRequest("Associated user not found.");
+                }
+                if (!string.IsNullOrEmpty(entity.Body) && entity.Body.Length > 20)
+                {
+                    // Cortar el cuerpo a 97 caracteres y agregar "..."
+                    entity.Body = entity.Body.Length > 97 ? entity.Body.Substring(0, 97) + "..." : entity.Body;
+                }
+
+                switch (entity.Type)
+                {
+                    case 1:
+                        entity.Category = "Farándula";
+                        break;
+                    case 2:
+                        entity.Category = "Política";
+                        break;
+                    case 3:
+                        entity.Category = "Futbol";
+                        break;
+                }
+
+                PostService.Create(entity);
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = "Post create" });
+            }
+            catch(Exception ex) 
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
+
+        }
+
+        [HttpPut()]
+        public PostEntity Update([FromBodyAttribute] PostEntity entity)
+        {
+            return PostService.Update(entity.PostId,entity, out bool changed);
+        }
+
+        [HttpDelete()]
+        public PostEntity Delete([FromBodyAttribute] PostEntity entity)
+        {
+            return PostService.Delete(entity);
+        }
+
+
+    }
+}
